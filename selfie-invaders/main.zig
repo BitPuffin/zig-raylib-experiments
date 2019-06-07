e68b1baac2112a7be98c8d7b3b9a9091381e4eab
Isak Andersson <isak@bobby.se>
2019-06-07 23:07:25 +0200

    rescale image and draw the faces

--- selfie-invaders/main.zig
+++ selfie-invaders/main.zig
@@ -1,30 +1,24 @@
 const std = @import("std");
+const mem = std.mem;
 const warn = std.debug.warn;
-
-// const ray = @cImport({
-//     // @cDefine("_NO_CRT_STDIO_INLINE", "1");
-//     // @cDefine("_GNU_SOURCE", "1");
-//     // @cDefine("PLATFORM", "PLATFORM_DESKTOP");
-//     @cInclude("raylib.h");
-// });
 
 const ray = @import("ray");
 
 const screenWidth = 1280;
-const screenHeight = 720;
+const screenHeight = 960;
 
 const PersonCount = 16;
+const PersonPadding = 30;
+
 const MaxBullets = 8;
 const MaxPersonBullets = 4;
 
 const cameraSpeed = 16;
 
-const screenMinOffset = 100;
-const screenMaxOffset = 250;
-const cameraMin = 0 + screenMinOffset;
-const cameraMax = screenWidth - screenMaxOffset;
+const screenOffset = 100;
+const cameraMin = 0 + screenOffset;
 
-const PersonState = enum {
+const PersonState = packed enum(u2) {
     ALIVE,
     DYING,
     DEAD
@@ -37,33 +31,69 @@ const GameState = struct {
 };
 
 pub fn main() void {
-    warn("Initializing...\n");
     ray.InitWindow(screenWidth, screenHeight, c"Selfie Invaders");
-
     ray.SetTargetFPS(60);
 
-    var state: GameState = undefined;
-    state.playerPos = 400;
+    var cameraTex = ray.LoadTexture(c"selfie-invaders/camera.png");
+    const cameraMax = screenWidth - (cameraTex.width + screenOffset);
 
-    var cameraImg = ray.LoadTexture(c"selfie-invaders/camera.png");
-    var wearyFaceImg = ray.LoadTexture(c"selfie-invaders/weary-face.png");
+    var state: GameState = undefined;
+    state.playerPos = @divFloor((screenWidth - cameraTex.width), 2); // center
+    {   // set zero
+        mem.secureZero(f64, state.timeSinceDeaths[0..]);
+        mem.secureZero(PersonState, state.personStates[0..]);
+    }
+
+    var wearyFaceTex = ray.LoadTexture(c"selfie-invaders/weary-face.png");
 
     while (!ray.WindowShouldClose()) {
-        ray.BeginDrawing();
-        ray.ClearBackground(ray.RAYWHITE);
+        {   // drawing
+            ray.BeginDrawing();
+            
+            ray.ClearBackground(ray.RAYWHITE);
+            
+            ray.DrawFPS(100,0);
+            ray.DrawTexture(cameraTex,
+                            @intCast(c_int, state.playerPos),
+                            screenHeight - cameraTex.height - PersonPadding,
+                            ray.WHITE);
 
-        ray.DrawTexture(cameraImg, @intCast(c_int, state.playerPos), 550, ray.WHITE);
+            {   // draw faces
+                var row: c_int = 0;
+                var xOffset: c_int = 0;
+                for(state.personStates) |s, idx| {
+                    const ci = @intCast(c_int, idx);
+                    if(idx > 0 and idx % 8 == 0) {
+                        row += 1;
+                        xOffset = 0;
+                    }
+                    if(s == PersonState.ALIVE) {
+                        const y = PersonPadding +
+                            row * (wearyFaceTex.height + PersonPadding);
+                        ray.DrawTexture(
+                            wearyFaceTex,
+                            PersonPadding + xOffset,
+                            y,
+                            ray.WHITE);
+                    }
+                    xOffset += PersonPadding + wearyFaceTex.width;
+                }
+            }
 
-        if(ray.IsKeyDown(ray.lib.KEY_LEFT)) {
-            state.playerPos -= cameraSpeed;
+            ray.EndDrawing();
         }
-        if(ray.IsKeyDown(ray.lib.KEY_RIGHT)) {
-            state.playerPos += cameraSpeed;
+
+        {   // update
+            if(ray.IsKeyDown(ray.lib.KEY_LEFT)) {
+                state.playerPos -= cameraSpeed;
+            }
+            if(ray.IsKeyDown(ray.lib.KEY_RIGHT)) {
+                state.playerPos += cameraSpeed;
+            }
+
+            if(state.playerPos < cameraMin) state.playerPos = cameraMin;
+            if(state.playerPos > cameraMax) state.playerPos = cameraMax;
         }
-
-        if(state.playerPos < cameraMin) state.playerPos = cameraMin;
-        if(state.playerPos > cameraMax) state.playerPos = cameraMax;
-
-        ray.EndDrawing();
+        warn("time: {}\n", ray.GetFrameTime());
     }
 }
